4277a60f08bcc4125b0956ed921405bd416cabd2
beethogedeon <beethovengedeon@gmail.com>
2024-02-22 20:33:09 +0100

    Fix errors

--- {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.pkg_name }}.py
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.pkg_name }}.py
@@ -1,3 +1,15 @@
 from __future__ import annotations
 
 from {{cookiecutter.pkg_name}}.hello_world import hello_world
+
+def hello_test():
+    hello_world()
+    
+
+def test_hello(unit_tests_mocks: None):
+    hello_test()
+ 
+    
+def test_init_hello():
+    hello_test()
+    
